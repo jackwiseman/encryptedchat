c3ed394686fcdc33933eafe26232d554d82130c4
jackwiseman <jackwiseman4@gmail.com>
2021-11-12 18:50:10 -0500

    send to server and print in console

--- client.go
+++ client.go
@@ -4,29 +4,68 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"os"
+	"os/exec"
 )
 
+// TODO: Next two functions
+
+/*func displayMessage(ch chan string) {
+	for {
+		fmt.Println(<-ch)
+	}
+}
+
+func listener(server net.Conn, ch chan string) {
+	b := bufio.NewReader(server)
+	for { // should only break when client disconnects
+		line, err := b.ReadString('\n')
+		if err != nil {
+			break
+		}
+		ch <- line
+	}
+}*/
+
+func sendToServer(server net.Conn, msg []byte) {
+	server.Write([]byte(msg))
+	fmt.Printf("To server -> " + string(msg) + "\n")
+}
+
 func main() {
+	// Handle connection
 	conn, err := net.Dial("tcp", ":3540")
 	if err != nil {
 		fmt.Printf("Did not connect")
 	} else {
-		fmt.Printf("Connected, hooray!\n")
-		//var test string
-		//fmt.Scanf("%s", &test)
-		conn.Write([]byte("This is a mesage from the client"))
+		fmt.Printf("Connected to server\n")
 	}
 
-	b := bufio.NewReader(conn)
+	// This next section is mostly unneccessary for now, deals with
+	// being able to have user input while messages are coming in
 
+	var input []byte
+	reader := bufio.NewReader(os.Stdin)
+	// Disable input buffering
+	exec.Command("stty", "-F", "/dev/tty/", "cbreak", "min", "1").Run()
+	
+	// Append each char to input byte
+
+	// Keeps connection open indefinitely so user can send msgs
 	for {
-		line, err := b.ReadString('\n')
-		if err != nil { // EOF, or worse
-			break
+		b, err := reader.ReadByte()
+		if err != nil {
+			panic(err)
+		} else {
+			if b == 10 {// newline
+				sendToServer(conn, input)
+				input = nil
+			} else {
+				input = append(input, b)
+			}
 		}
-		//client.Write(line)
-		fmt.Printf(line)
 	}
 
 	conn.Close()
+	fmt.Printf("Disconnected from server ")
 }
--- server.go
+++ server.go
@@ -4,22 +4,33 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"net"
 	"strconv"
+	"io"
 )
 
 const PORT = 3540
 
+func reciever(ch chan string) {
+	for {
+		fmt.Println(<-ch)
+	}
+}
+
 func main() {
+
 	server, err := net.Listen("tcp", ":" + strconv.Itoa(PORT))
 	if server == nil {
 		panic("couldn't start listening: " + err.Error())
 	}
+	
+	ch := make(chan string)
+
 	conns := clientConns(server)
 	for {
-		go handleConn(<-conns)
+		go reciever(ch)
+		go handleConn(<-conns, ch)
 	}
 }
 
@@ -41,17 +52,23 @@ func clientConns(listener net.Listener) chan net.Conn {
 	return ch
 }
 
-func handleConn(client net.Conn) {
+func handleConn(client net.Conn, ch chan string) {
+
+	buf := make([]byte, 0, 4096)
+	tmp := make([]byte, 256)
+
 	for {
-		b := bufio.NewReader(client)
-		line, err := b.ReadString('\n')
-		if err != nil { // EOF, or worse
+		n, err := client.Read(tmp)
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println("read error")
+			}
 			break
 		}
-		//client.Write(line)
-		fmt.Printf(line)
-		response(client, line)
+		buf = append(buf, tmp[:n]...)
+		ch <- string(buf)
 	}
+
 }
 
 func response(client net.Conn, line string) {
